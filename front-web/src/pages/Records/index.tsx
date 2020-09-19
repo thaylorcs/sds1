8ba458195a4e91cafb2e50b02bc68a6aa5525a9b
Thaylor Carvalho <thaylorcs@outlook.com>
2020-09-19 17:38:20 -0300

    Criando página de listagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles.css';
+import {RecordsResponse} from './types'
+import { formatDate } from './helpers';
+import Pagination from './Pagination';
+import {Link} from 'react-router-dom'
 
-const Records = () => (
-    <h1>Records</h1>
-);
+const BASE_URL = 'http://localhost:8080'
+
+const Records = () => {
+    // criando um estado interno pro componente  - React Hooks e armazena o valor pra popular a tabela
+    const [ recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+
+    const[activePage, setActivePage] = useState(0);
+
+    // executa a requisição
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+        .then(response => setRecordsResponse(response.data));
+    }, [activePage]);
+
+    const handlePageChange = (index: number)  => {
+        setActivePage(index)
+    }
+
+   return (
+        <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">
+                        Ver gráficos
+                    </button>
+                </Link>
+            </div>
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>Instante</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Plataforma</th>
+                        <th>Gênero</th>
+                        <th>Título</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsResponse?.content.map(record => (
+                                <tr key={record.id}>
+                                    <td>{formatDate(record.moment)}</td>
+                                    <td>{record.name}</td>
+                                    <td>{record.age}</td>
+                                    <td className="text-secondary">{record.gamePlatform}</td>
+                                    <td>{record.genreName}</td>
+                                    <td className="text-primary">{record.gameTitle}</td>
+                                </tr>
+                            )
+                        )
+                    }                    
+                </tbody>
+            </table>
+            <Pagination 
+                activePage={activePage}
+                goToPage={handlePageChange}
+                totalPages={recordsResponse?.totalPages}
+                />
+        </div>
+    );
+}
 
 export default Records;
